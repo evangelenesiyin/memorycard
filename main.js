637388cb4ca2fbd3ed158c2f1687ce85ce2e6968
Evangelene <siyinyap@gmail.com>
2023-08-28 11:24:44 +0800

    feat: shuffle card deck upon clicking normal button
    fix: use concat to duplicate elements in the same fruit array

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-const fruitsArray = [
-  "assets/avocado.png",
-  "assets/blueberry.png",
-  "assets/coconut.png",
-  "assets/kiwi.png",
-  "assets/lemon.png",
-  "assets/mango.png",
-  "assets/orange.png",
-  "assets/watermelon.png",
-  "assets/avocado.png",
-  "assets/blueberry.png",
-  "assets/coconut.png",
-  "assets/kiwi.png",
-  "assets/lemon.png",
-  "assets/mango.png",
-  "assets/orange.png",
-  "assets/watermelon.png",
+let fruitsArray = [
+  "public/assets/avocado.png",
+  "public/assets/blueberry.png",
+  "public/assets/coconut.png",
+  "public/assets/kiwi.png",
+  "public/assets/lemon.png",
+  "public/assets/mango.png",
+  "public/assets/orange.png",
+  "public/assets/watermelon.png",
 ];
 
 let matchedCards = 0;
@@ -29,16 +21,34 @@ const card = document.querySelectorAll(".frontview");
 normalButton.addEventListener("click", startNormal);
 hardButton.addEventListener("click", startHard);
 
+// ================================================
+
+// starts normal mode - duplicates the array of existing cards and shuffles the deck
 function startNormal() {
   normalMode.classList.remove("none");
   mainMenu.style.display = "none";
+  normalDeck(fruitsArray);
+  shuffleArray(fruitsArray);
 }
 
+// duplicating elements in fruitsArray to fit 4 x 4 grid
+function normalDeck() {
+  fruitsArray = fruitsArray.concat(fruitsArray);
+  return fruitsArray;
+}
+
+// ================================================
+
+// starts hard mode
 function startHard() {
   hardMode.classList.remove("none");
   mainMenu.style.display = "none";
+  shuffleArray(fruitsArray);
 }
 
+// ================================================
+
+// create image elements
 function appendFruit(index) {
   const addFruit = document.createElement("img");
   addFruit.src = fruitsArray[index];
@@ -48,9 +58,18 @@ function appendFruit(index) {
   return addFruit;
 }
 
+// append image elements in each grid upon clicking
 for (let i = 0; i < card.length; i++) {
   card[i].addEventListener("click", function () {
     card[i].innerHTML = "";
     card[i].append(appendFruit(i));
   });
+}
+
+// shuffle deck
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
 }
